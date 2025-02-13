d56a7eae595bc5be2b749a7de79f81e2c67b9c39
ssandyy <ssandyy789@gmail.com>
2025-02-13 11:06:41 +0530

    initial

---
+++ src/main/java/org/feb2025/App.java
@@ -0,0 +1,13 @@
+package org.feb2025;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Hibernate!" );
+    }
+}
