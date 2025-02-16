664c216c7f78f6ea993c6e08e4357338d6f32410
ssandyy <ssandyy789@gmail.com>
2025-02-16 08:00:51 +0530

    2

--- src/main/java/org/feb2025/App.java
+++ src/main/java/org/feb2025/App.java
@@ -1,5 +1,10 @@
 package org.feb2025;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
 /**
  * Hello world!
  *
@@ -8,6 +13,24 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Hibernate!" );
+        Avatar av1 = new Avatar();
+        av1.setAid(001);
+        av1.setAname("Valentine");
+        av1.setTech("Human");
+
+
+        Configuration configuration = new Configuration();
+        configuration.addAnnotatedClass(org.feb2025.Avatar.class);
+        configuration.configure("hibernate.cfg.xml");
+
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+
+        session.beginTransaction();
+        session.merge(av1); // Use merge() instead of persist()
+        session.getTransaction().commit();
+
+        session.close();
+
     }
 }
--- src/main/java/org/feb2025/Avatar.java
+++ src/main/java/org/feb2025/Avatar.java
@@ -1,5 +1,50 @@
 package org.feb2025;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Avatar {
+
+    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int aid;
+    private String aname;
+    private String tech;
+
+    public int getAid() {
+        return aid;
+    }
+
+    public void setAid(int aid) {
+        this.aid = aid;
+    }
+
+    public String getAname() {
+        return aname;
+    }
+
+    public void setAname(String aname) {
+        this.aname = aname;
+    }
+
+    public String getTech() {
+        return tech;
+    }
+
+    public void setTech(String tech) {
+        this.tech = tech;
+    }
+
+    @Override
+    public String toString() {
+        return "Avatar{" +
+                "aid=" + aid +
+                ", aname='" + aname + '\'' +
+                ", tech='" + tech + '\'' +
+                '}';
+    }
 
 }
